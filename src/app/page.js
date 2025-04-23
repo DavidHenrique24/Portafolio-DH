e33cdeb037c27e3d9ad64131b14a67b4b3f82f36
DavidHenrique24 <henriquemaciasdavidalejandro@fpllefia.com>
2025-04-23 20:55:10 +0200

    .

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,30 @@
-import './globals.css';
+
 export default function Home() {
   return (
-    <main className=''>
-      {/* Menu inicio  */}
-      <div> 
-      <ul className="menuInicio flex justify-center gap-20 list-none p-0 mt-5">
-      <li><a href="#">Home</a></li>
-      <li>//</li>
-      <li><a href="#">Acerca de mi</a></li>
-      <li>//</li>
-    <li><a href="#">Experiencia</a></li>
-    <li>//</li>
-    <li><a href="#">Proyecto</a></li>
-    </ul>
-    </div>
+    <main className="min-h-screen bg-gradient-to-b from-purple-900 to-black text-white">
+      {/* Menu inicio */}
+      <div className="flex flex-col items-center justify-center h-[10vh] text-center px-2">
+        <ul className="menuInicio flex justify-center gap-20 list-none  ">
+          <li><a href="#">Home</a></li>
+          <li>//</li>
+          <li><a href="#">Acerca de mi</a></li>
+          <li>//</li>
+          <li><a href="#">Experiencia</a></li>
+          <li>//</li>
+          <li><a href="#">Proyecto</a></li>
+        </ul>
+      </div>
 
-      {/* Home  */}
-      <div className="relative">
-  <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: "url('https://preview.redd.it/1iwcit1gidyy.jpg?auto=webp&s=2e7883b8b95e0d5ed00e66d862992b618b8a3796')" }}></div>
-  <div className="relative z-10 text-white p-8">
-    <h1 className="text-4xl">Texto sobre la imagen</h1>
-    <p className="mt-4">Este es un párrafo encima de la imagen de fondo.</p>
-  </div>
-</div>
-
-
-
+      {/* Home */}
+      <div className="flex justify-center items-center h-[80vh]">
+        <div
+          className="w-[600px] h-[400px] bg-cover bg-center flex flex-col items-center justify-center text-center px-4 rounded-lg shadow-lg"
+          style={{ backgroundImage: "url('/img/cubo.png')" }}
+        >
+          <h1 className="text-5xl font-bold">David Henrique</h1>
+          <p className="mt-4 text-lg max-w-md">Este es un párrafo encima de un fondo que va de morado a negro.</p>
+        </div>
+      </div>
     </main>
   );
 }
-
